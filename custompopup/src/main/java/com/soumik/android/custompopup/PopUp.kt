19607c7b7ac60cf449e641fe85b9cf4b21b4e5b5
SoumikBhatt <soumikbhatt95@gmail.com>
2019-12-12 17:40:18 +0600

    custom_pop_up_added

--- custompopup/src/main/java/com/soumik/android/custompopup/PopUp.kt
+++ custompopup/src/main/java/com/soumik/android/custompopup/PopUp.kt
@@ -37,7 +37,6 @@ class PopUp {
             dialog.show()
         }
 
-
         fun error(activity: Activity,heading:String,message:String){
 
             val dialog = Dialog(activity)
@@ -52,6 +51,29 @@ class PopUp {
             val headingTV = dialog.findViewById<TextView>(R.id.b)
             headingTV.text=heading
             headingTV.setTextColor(Color.parseColor("#E2574C"))
+            val titleTV = dialog.findViewById<TextView>(R.id.text_dialog)
+            titleTV.text=message
+
+            val okButton = dialog.findViewById<Button>(R.id.btn_dialog)
+            okButton.setOnClickListener { dialog.dismiss() }
+
+            dialog.show()
+        }
+
+        fun customNotification(activity: Activity,heading:String,message:String,icon:Int) {
+
+            val dialog = Dialog(activity)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.setContentView(R.layout.custom_pop_up)
+
+            val imageView = dialog.findViewById<ImageView>(R.id.a)
+            imageView.setImageResource(icon)
+
+            val headingTV = dialog.findViewById<TextView>(R.id.b)
+            headingTV.text=heading
+            headingTV.setTextColor(Color.parseColor("#006064"))
             val titleTV = dialog.findViewById<TextView>(R.id.text_dialog)
             titleTV.text=message
 
