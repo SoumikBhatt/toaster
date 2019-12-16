b83b6a75be6f3b82fde1c903ff313fa84ca6dada
SoumikBhatt <soumikbhatt95@gmail.com>
2019-12-17 00:36:31 +0600

    update_in_exit

--- custompopup/src/main/java/com/soumik/android/custompopup/Popup.kt
+++ custompopup/src/main/java/com/soumik/android/custompopup/Popup.kt
@@ -13,63 +13,69 @@ import kotlinx.android.synthetic.main.custom_exit.*
 
 class Popup {
 
-    companion object{
+    companion object {
 
 
-        fun success(activity: Activity,heading:String,message:String,font: Int){
+        fun success(activity: Activity, heading: String, message: String, font: Int) {
 
             val dialog = Dialog(activity)
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
             dialog.setCancelable(false)
             dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.setContentView(R.layout.custom_pop_up)
-            val typeFace = ResourcesCompat.getFont(activity,font)
+            val typeFace = ResourcesCompat.getFont(activity, font)
             val imageView = dialog.findViewById<ImageView>(R.id.a)
             imageView.setImageResource(R.drawable.ic_checked)
 
             val headingTV = dialog.findViewById<TextView>(R.id.b)
-            headingTV.text=heading
+            headingTV.text = heading
             headingTV.setTextColor(Color.parseColor("#0DEB43"))
-            headingTV.typeface=typeFace
+            headingTV.typeface = typeFace
             val titleTV = dialog.findViewById<TextView>(R.id.text_dialog)
-            titleTV.text=message
-            titleTV.typeface=typeFace
+            titleTV.text = message
+            titleTV.typeface = typeFace
 
             val okButton = dialog.findViewById<Button>(R.id.btn_dialog)
-            okButton.typeface=typeFace
+            okButton.typeface = typeFace
             okButton.setOnClickListener { dialog.dismiss() }
 
             dialog.show()
         }
 
-        fun error(activity: Activity,heading:String,message:String,font: Int){
+        fun error(activity: Activity, heading: String, message: String, font: Int) {
 
             val dialog = Dialog(activity)
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
             dialog.setCancelable(false)
             dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.setContentView(R.layout.custom_pop_up)
-            val typeFace = ResourcesCompat.getFont(activity,font)
+            val typeFace = ResourcesCompat.getFont(activity, font)
 
             val imageView = dialog.findViewById<ImageView>(R.id.a)
             imageView.setImageResource(R.drawable.ic_red_alert)
 
             val headingTV = dialog.findViewById<TextView>(R.id.b)
-            headingTV.text=heading
+            headingTV.text = heading
             headingTV.setTextColor(Color.parseColor("#E2574C"))
-            headingTV.typeface=typeFace
+            headingTV.typeface = typeFace
             val titleTV = dialog.findViewById<TextView>(R.id.text_dialog)
-            titleTV.text=message
-            titleTV.typeface=typeFace
+            titleTV.text = message
+            titleTV.typeface = typeFace
 
             val okButton = dialog.findViewById<Button>(R.id.btn_dialog)
-            okButton.typeface=typeFace
+            okButton.typeface = typeFace
             okButton.setOnClickListener { dialog.dismiss() }
 
             dialog.show()
         }
 
-        fun customPopup(activity: Activity,heading:String,message:String,icon:Int,font:Int) {
+        fun customPopup(
+            activity: Activity,
+            heading: String,
+            message: String,
+            icon: Int,
+            font: Int
+        ) {
 
             val dialog = Dialog(activity)
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -78,27 +84,27 @@ class Popup {
             dialog.setContentView(R.layout.custom_pop_up)
 
 
-            val typeFace = ResourcesCompat.getFont(activity,font)
+            val typeFace = ResourcesCompat.getFont(activity, font)
 
             val imageView = dialog.findViewById<ImageView>(R.id.a)
             imageView.setImageResource(icon)
 
             val headingTV = dialog.findViewById<TextView>(R.id.b)
-            headingTV.text=heading
+            headingTV.text = heading
             headingTV.setTextColor(Color.parseColor("#006064"))
             headingTV.typeface = typeFace
             val titleTV = dialog.findViewById<TextView>(R.id.text_dialog)
-            titleTV.text=message
-            titleTV.typeface=typeFace
+            titleTV.text = message
+            titleTV.typeface = typeFace
 
             val okButton = dialog.findViewById<Button>(R.id.btn_dialog)
-            okButton.typeface=typeFace
+            okButton.typeface = typeFace
             okButton.setOnClickListener { dialog.dismiss() }
 
             dialog.show()
         }
 
-        fun exit(activity: Activity,heading: String,subTitle: String){
+        fun exit(activity: Activity, heading: String, subTitle: String) {
 
             val dialog = Dialog(activity)
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -107,13 +113,16 @@ class Popup {
             dialog.setContentView(R.layout.custom_exit)
 
             val headingTV = dialog.tv_heading
-            headingTV.text=heading
+            headingTV.text = heading
 
             val titleTV = dialog.tv_sub_title
-            titleTV.text=subTitle
+            titleTV.text = subTitle
 
             val ok = dialog.iv_ok
-            ok.setOnClickListener { activity.finishAffinity() }
+            ok.setOnClickListener {
+                activity.finishAffinity()
+                dialog.dismiss()
+            }
 
             val cancel = dialog.iv_cancel
             cancel.setOnClickListener { dialog.dismiss() }
